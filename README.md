b3a1ce1cc2f1c11862e290bd0f23edd2a7cea103
Anderson Siqueira <contato@andersonls.com.br>
2020-06-19 22:42:38 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# cursoLaravel
+ Curso de Introdução ao Laravel -  https://www.udemy.com/course/introducao-ao-laravel-53/
---
+++ routes/web.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/contato/{id?}', function($id = null){
+	return "contato id = $id";
+});
+
+Route::post('/contato', function(){
+	dd($_POST);
+	return "contato POST";
+});
+
+Route::put('/contato', function(){
+
+	return "contato PUT";
+});
+
+
